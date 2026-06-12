238451d2fae4252a6cded76670efb1ee35e934b3
abdulrehmanmazhar <abdulrehmanmazhar02@gmail.com>
2026-06-13 00:42:03 +0500

    subarray sum from optimized brute force loop

--- subarray/main.cpp
+++ subarray/main.cpp
@@ -2,17 +2,19 @@
 #include <vector>
 using namespace std;
 
+// brute force optimized
+
 int main() {
     int n = 5;    
     int arr[5] = {1, 2, 3, 4, 5};
 
+    int maxSum = INT_MIN;
     for (int st=0; st<n; st++){
+        int currSum = 0;
         for (int end=st; end<n; end++){
-            for (int i=st; i<=end; i++){
-                cout << arr[i];
-            }
-            cout << " ";
+            currSum += arr[end];
+            maxSum = max(currSum, maxSum);
         }
-        cout << endl;
     }
+    cout << "max subarray sum = " << maxSum << endl;
 }
