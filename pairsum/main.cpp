7024ae716cdd1382c2654cac703ffd871f2f9e12
abdulrehmanmazhar <abdulrehmanmazhar02@gmail.com>
2026-06-13 01:53:40 +0500

    pair sum optimized

--- pairsum/main.cpp
+++ pairsum/main.cpp
@@ -22,12 +22,45 @@ vector<int> pairSumBruteForce(vector<int> nums, int target)
 
     return ans;
 }
+
+vector<int> pairSumOptimized(vector<int> nums, int target)
+{
+    vector<int> ans;
+    int n = nums.size();
+
+    int i = 0, j = n-1;
+
+    while(i<j){
+        int pairSum = nums[i] + nums[j];
+        if(pairSum>target){
+            j--;
+        }else if(pairSum<target){
+            i++;
+        }else{
+            ans.push_back(i);
+            ans.push_back(j);
+            return ans;
+        }
+    }
+    return ans;
+}
+
+
 int main()
 {
     vector<int> nums = {2, 7, 11, 15};
     int target = 9;
+    bool isSorted = true;
 
-    vector<int> ans = pairSumBruteForce(nums, target);
+    vector<int> ans;
+
+    if(isSorted){
+        ans =   pairSumBruteForce(nums, target);
+    }else{
+        // only optimized approach will work here as the array is sorted ascending order
+         ans = pairSumOptimized(nums, target);
+    }
+
 
     if (ans.size() >= 2)
     {
