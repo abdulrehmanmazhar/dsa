9ab5350cb88b12842978a8db471cc5598d3e683a
abdulrehmanmazhar <abdulrehmanmazhar02@gmail.com>
2026-06-16 23:18:04 +0500

    majority element optimized and moore's algorithm

--- majorityelement/main.cpp
+++ majorityelement/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 int majorityElementBruteForce(vector<int> nums)
@@ -27,11 +28,70 @@ int majorityElementBruteForce(vector<int> nums)
     return -1;
 }
 
+int majorityElementOptimized(vector<int> nums)
+{
+    int n = nums.size();
+
+    sort(nums.begin(), nums.end());
+
+    int freq = 1, ans = nums[0];
+
+    for (int i = 1; i < n; i++)
+    {
+        if (nums[i] == nums[i - 1])
+        {
+            freq++;
+        }
+        else
+        {
+            ans = nums[i];
+            freq = 1;
+        }
+
+        if (freq > n / 2)
+        {
+            return ans;
+        }
+    }
+    return -1;
+}
+
+int majorityElementMooreAlgo(vector<int> nums)
+{
+    int n = nums.size();
+
+
+    int freq = 0, ans = 0;
+
+    for (int i = 1; i < n; i++)
+    {
+        if (freq==0)
+        {
+            ans = nums[i];
+            freq = 1;
+        }
+        else if (nums[i] == ans)
+        {
+            freq++;
+        }
+        else
+        {
+            freq--;
+        }
+
+        if (freq > n / 2)
+        {
+            return ans;
+        }
+    }
+    return -1;
+}
+
 int main()
 {
     vector<int> nums = {2, 7, 11, 15, 15, 15, 15};
-    
-    int majorityElement = majorityElementBruteForce(nums);
+
+    int majorityElement = majorityElementMooreAlgo(nums);
 
     cout << "majority element = " << majorityElement << endl;
 }
